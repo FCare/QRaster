547b46d31c31d9123d4f1f81bc9f4395a33caa99
LeCare <francois.caron.perso@gmail.com>
2019-07-15 18:21:44 +0200

    Compile and show a window

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+/***************************************************************************/
+/* Francois CARON - 15/07/2019                                             */
+/* QRaster is intented to implement the Sega saturn VDP1 rasterizer and    */
+/* Texture mapper as a compute shader in order to avoid issu of converting */
+/* Quads to triangles and the processing of the openGL coordinates         */
+/***************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+
 #if defined(_USEGLEW_)
 #include <GL/glew.h>
 #endif
@@ -11,7 +21,7 @@ static void error_callback(int error, const char* description)
   fputs(description, stderr);
 }
 
-static int platform_SetupOpenGL(int w, int h, int fullscreen) {
+static int setupOpenGL(int w, int h) {
   int i;
   if (!glfwInit())
     return 0;
@@ -21,7 +31,6 @@ static int platform_SetupOpenGL(int w, int h, int fullscreen) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-  glfwWindowHint(GLFW_OPENGL_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RED_BITS,8);
   glfwWindowHint(GLFW_GREEN_BITS,8);
@@ -33,7 +42,7 @@ static int platform_SetupOpenGL(int w, int h, int fullscreen) {
   if (!g_window)
   {
     glfwTerminate();
-    return 0;
+    return -1;
   }
 
   glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
@@ -41,6 +50,10 @@ static int platform_SetupOpenGL(int w, int h, int fullscreen) {
   glfwSwapInterval(0);
 #if defined(_USEGLEW_)
   glewExperimental=GL_TRUE;
+  if (glewInit() != 0) {
+    printf("Glew can not init\n");
+    return -1;
+  }
 #endif
 
    int maj, min;
@@ -48,14 +61,43 @@ static int platform_SetupOpenGL(int w, int h, int fullscreen) {
    glGetIntegerv(GL_MINOR_VERSION, &min);
 
   printf("OpenGL version is %d.%d (%s, %s)\n", maj, min, glGetString(GL_VENDOR), glGetString(GL_RENDERER));
-  return 1;
+
 }
 
-static int SetupOpenGL(int w, int h) {
-  if (!platform_SetupOpenGL(w,h,fullscreen))
-    exit(EXIT_FAILURE);
+static void releaseOpenGL() {
+  glfwSetWindowShouldClose(g_window, GL_TRUE);
+  glfwDestroyWindow(g_window);
+  glfwTerminate();
+}
+
+static int shouldClose() {
+  return glfwWindowShouldClose(g_window);
+}
+
+static void pollEvents() {
+  glfwPollEvents();
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+static void setKeyCallback() {
+  glfwSetKeyCallback(g_window, key_callback);
 }
 
 int main(int argc, char *argv[]) {
-  SetupOpenGL(600, 480);
+
+  setupOpenGL(600, 480);
+
+  setKeyCallback();
+
+  while (!shouldClose())
+  {
+       pollEvents();
+  }
+
+  releaseOpenGL();
 }
